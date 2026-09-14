021fa345c012b560ea90332277088a57912bd3b5
Alexander Jonsson <alexanderjonsson95@gmail.com>
2026-09-14 13:13:32 +0200

    First commit, SFML installed.

---
+++ sfmlProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+namespace sfmlProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var window = new RenderWindow(new VideoMode(500, 700), "breakout"))
+        {
+            window.Closed += (o, e) => window.Close();
+            Clock clock = new Clock();
+            while (window.IsOpen)
+            {
+                float deltaTime = clock.Restart().AsSeconds();
+                window.DispatchEvents();
+                // TODO: Updates
+                window.Clear(new Color(131, 197, 235));
+                // TODO: Drawing
+                window.Display();
+            }
+        }
+    }
+}
