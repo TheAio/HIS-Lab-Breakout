b7a7e414a35df44c1e72bddcd8167ed9294cc249
TheAio <76646135+TheAio@users.noreply.github.com>
2026-09-17 12:03:08 +0200

    gitignore?

--- sfmlProject/obj/Debug/net10.0/sfmlProject.AssemblyInfo.cs
+++ sfmlProject/obj/Debug/net10.0/sfmlProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sfmlProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a29507dbb684322a72db1fc8619bdf906f026ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14fa2bf688b852e18fb84ef666971756295b2d0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("sfmlProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sfmlProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
