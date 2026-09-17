25f541ce2a42efbcd202ceee36e29f6ea8977df6
Alexander Jonsson <alexanderjonsson95@gmail.com>
2026-09-17 10:38:55 +0200

    pause pause pause

---
+++ sfmlProject/Pause.cs
@@ -0,0 +1,55 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace sfmlProject;
+
+public class Pause
+{
+    // ReSharper disable once InconsistentNaming
+    public bool isPaused = true; //TODO: revert to false
+    public Text gui = new();
+    public Ball ball = new();
+    
+  public void PauseGame()
+    {
+        isPaused = true;
+    }
+
+    public void Update(RenderWindow window, Ball ball, Tiles tiles)
+    {
+        window.KeyPressed += (o, e) =>
+        {
+            //TODO Change space to another key it is TEMPORARY
+            if (e.Code == Keyboard.Key.Space && isPaused)
+            {
+                ball.health = 3;
+                ball.score = 0;
+                tiles.CreateTiles();
+                
+                isPaused = false;
+            }
+        };
+    }
+
+    public void Draw(RenderTarget target)
+    {
+        if (ball.health > 0)
+        {
+            gui.DisplayedString = "The game is now paused!";
+        }
+        else
+        {
+            gui.DisplayedString = "You are dead!";
+        }
+        gui.Position = new Vector2f(12, 8);
+        target.Draw(gui);
+    }
+
+    public Pause()
+    {
+        gui = new Text();
+        gui.CharacterSize = 24;
+        gui.Font = new Font("assets/future.ttf");
+    }
+}
--- sfmlProject/obj/Debug/net10.0/sfmlProject.AssemblyInfo.cs
+++ sfmlProject/obj/Debug/net10.0/sfmlProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sfmlProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+117c94bdbdbf43a5c68b2347fa09d2bd838a3f16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad6e28b5dd036b381d562825f13cbb87848a1c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("sfmlProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sfmlProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
